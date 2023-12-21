52f71f82a3dfacba4037600ab1ec783714a78eb6
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2023-12-21 15:20:01 -0500

    put special characters for the password

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+console.log(this);
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Array of characters that are special included in password
+var specialCharacters = ['+','=',')','(','*','&','^','%','$','#','@','!','?','/','|',':','{','}','\','>','<',','_','-', ];
+
+//Array of uppercase characters included in the password
+var uppercasedCharacters = 
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
