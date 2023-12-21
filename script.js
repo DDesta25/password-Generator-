702c81e9c03721225d8dff4b786511e10bf705ba
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2023-12-21 15:30:12 -0500

    added in capital and lowercase passwords

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ var generateBtn = document.querySelector("#generate");
 // Array of characters that are special included in password
 var specialCharacters = ['+','=',')','(','*','&','^','%','$','#','@','!','?','/','|',':','{','}','\','>','<',','_','-', ];
 
-//Array of uppercase characters included in the password
-var uppercasedCharacters = 
+// Array of uppercase characters included in the password
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+
+// Array of Lowercase characters included in password
+var LowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 
 function writePassword() {
